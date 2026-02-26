cd1e3ca9eea961e6f1c6064cac9a73889a824ab2
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-27 00:37:38 +0530

    set-up done

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import CounterReducer from "./CounterSlice";
 
-const store=configureStore({
-    reducer:
-})
+const store = configureStore({
+  reducer: CounterReducer,
+});
 
-
-export default store
+export default store;
