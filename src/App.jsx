45f9e2749e8d1be1cb61ead359355afd9d6834d8
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-27 02:54:47 +0530

    set-up done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./Navbar";
 
 function App() {
+
+  const [movie,setMovie]=useState()
   return (
     <>
       Hello
       <Navbar />
-      {/* <input></input> */}
+      <input value={movie} onChange={(e)=>setMovie(e.terget.value)}></input>
     </>
   );
 }
