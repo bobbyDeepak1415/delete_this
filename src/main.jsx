699b1adc21409b6f6c7e667a1e0e254717815b9e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-27 02:51:53 +0530

    set-up done

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Provider store={store}>
+
       <App />
+    </Provider>
   </StrictMode>,
 );
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,1 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
 
+import MoviesReducer from './MoviesSlice'
+
+const store=configureStore({
+  reducer:{
+    mmoviesApp:MoviesReducer
+  }
+})
+
+
+export default store
