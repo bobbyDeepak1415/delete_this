4ee1c4b0e48499a62e95c64566d791badc7de1e6
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-27 02:46:15 +0530

    set-up done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { addAmount, decrement, increment } from "./redux/CounterSlice";
+import Navbar from "./Navbar";
 
 function App() {
-  const count = useSelector((state) => state.counter.value);
-
-  const dispatch = useDispatch();
-
   return (
     <>
       Hello
-      <p>{count}</p>
-      <button onClick={() => dispatch(increment())}>+</button>
-      <button onClick={() => dispatch(decrement())}>-</button>
-      <button onClick={() => dispatch(addAmount(3))}>Add 3</button>
+      <Navbar />
+      {/* <input></input> */}
     </>
   );
 }
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,11 +1,1 @@
-import { configureStore } from "@reduxjs/toolkit";
 
-import CounterReducer from "./CounterSlice";
-
-const store = configureStore({
-  reducer: {
-    counter: CounterReducer,
-  },
-});
-
-export default store;
