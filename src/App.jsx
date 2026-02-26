bda1b73122a18baf7d1877dccaac187274cf2723
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-27 00:38:51 +0530

    set-up done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useSelector } from 'react-redux'
 import './App.css'
 
 function App() {
 
+
+  const count=useSelector(state=>state.counter.value)
+
+   const dispatch = useDispatch();
+
   return (
     <>
-     Hello
+     <p>{count}</p>
+     <button>+</button>
+     <button>-</button>
+     <button>Add 3</button>
     </>
   )
 }
