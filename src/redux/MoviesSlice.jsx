c1b202595849e56e6a78fb875ce936aedc8207e8
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-28 16:42:28 +0530

    setup done

--- src/redux/MoviesSlice.jsx
+++ src/redux/MoviesSlice.jsx
@@ -1,22 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  value: 0,
+  value: [
+    { id: 0, name: "Starwars" },
+    { id: 2, name: "Avatar" },
+  ],
 };
 
 const MoviesSlice = createSlice({
   name: "MoviesApp",
   initialState,
   reducers: {
-    addMovie:()=>{
-const newMovie={
-  id:
-
-}
+    addMovie: () => {
+      const newMovie = {
+        id: state.value.length,
+      };
     },
-    removeMovie:()=>{
-
-    }
+    removeMovie: () => {},
   },
 });
 
