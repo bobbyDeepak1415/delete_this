5ad7d46a5534453dadece631678dccdde05a4557
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-28 16:39:06 +0530

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./Navbar";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addMovie } from "./redux/MoviesSlice";
-
-
 
 function App() {
   const [movie, setMovie] = useState();
 
+  const movies = useSelector((state) => state.moviesApp.value);
+
   const dispatch = useDispatch();
 
+  const handleAddMovie = () => {
+    if (!movie.trim()) return;
 
-  const handleAddMovie=()=>{
-    if(!movie.trim()) return
-
-dispatch(addMovie(movie))
-setMovie("")
-
-
-
-
-  }
+    dispatch(addMovie(movie));
+    setMovie("");
+  };
 
   return (
     <>
@@ -29,9 +24,9 @@ setMovie("")
       {/* <Navbar /> */}
       <input value={movie} onChange={(e) => setMovie(e.target.value)}></input>
       <button onClick={handleAddMovie}>Add</button>
-
-{}
-
+      {movies.map((movie) => {
+        return <li key={movie.id}>{movie.name}</li>;
+      })}
     </>
   );
 }
